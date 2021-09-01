27cffe1d75a7bce7b746af8c916468a7607927ce
Me <mergorgec@gmail.com>
2021-09-01 17:43:55 +0200

    add flexible content layouts

--- template-parts/navigation/navigation-top.php
+++ template-parts/navigation/navigation-top.php
@@ -9,25 +9,25 @@
  */
 
 ?>
-<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentyseventeen' ); ?>">
+<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e('Top Menu', 'twentyseventeen');?>">
 	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
 		<?php
-		echo twentyseventeen_get_svg( array( 'icon' => 'bars' ) );
-		echo twentyseventeen_get_svg( array( 'icon' => 'close' ) );
-		_e( 'Menu', 'twentyseventeen' );
-		?>
+echo twentyseventeen_get_svg(array('icon' => 'bars'));
+echo twentyseventeen_get_svg(array('icon' => 'close'));
+_e('Menu', 'twentyseventeen');
+?>
 	</button>
 
 	<?php
-	wp_nav_menu(
-		array(
-			'theme_location' => 'top',
-			'menu_id'        => 'top-menu',
-		)
-	);
-	?>
+wp_nav_menu(
+    array(
+        'theme_location' => 'top',
+        'menu_id' => 'top-menu',
+    )
+);
+?>
 
-	<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
-		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
-	<?php endif; ?>
+	<?php if ((twentyseventeen_is_frontpage() || (is_home() && is_front_page())) && has_custom_header()): ?>
+		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg(array('icon' => 'arrow-right')); ?><span class="screen-reader-text"><?php _e('Scroll down to content', 'twentyseventeen');?></span></a>
+	<?php endif;?>
 </nav><!-- #site-navigation -->
